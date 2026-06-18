f84b0ece006bcc9a40fbdddb366a82c9a9e725fe
tudormobile <bill@tudormobile.com>
2026-06-18 12:10:20 -0400

    Update LICENSE, README, and package.json; add CI/CD workflow and vers… (#2)
    * Update LICENSE, README, and package.json; add CI/CD workflow and versioning script
    
    * Update README and version generation script; add displayName to version info and create .npmrc file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ZoneTracker
 GPS Fitness Activity Tracker
+[![Vue App](https://github.com/tudormobile/ZoneTracker/actions/workflows/web.yml/badge.svg)](https://github.com/tudormobile/ZoneTracker/actions/workflows/web.yml)
+
+## Project Structure
+- `doc` - Documentation
+- `vue-tracker` - Web app to track GPS activities
+- `vue-tracker-pin` - Cloudflare Worker that provides a PIN-based login flow
---
+++ vue-tracker/scripts/gen-version.js
@@ -0,0 +1,5 @@
+import { readFileSync, writeFileSync } from 'fs';
+const pkg = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), 'utf-8'));
+const version = process.env.VITE_APP_VERSION || pkg.version;
+const versionInfo = { displayName: pkg.displayName, name: pkg.name, version: version, author: pkg.author, copyright: pkg.copyright, license: pkg.license, buildDate: new Date() };
+writeFileSync(new URL('../public/version.json', import.meta.url), JSON.stringify(versionInfo, null, 2));
